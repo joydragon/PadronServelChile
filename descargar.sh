72fb002ed73a4cd3f14f474c1c3105c976708a3b
Ricardo Monreal <joydragon@gmail.com>
2017-08-22 21:47:37 -0300

    Agregando la opcion de solo descargar los PDF y mas texto informativo mientras funciona el script.

--- descargar.sh
+++ descargar.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#Necesitas los siguientes ejecutables:
+
+# 
+# Para este script necesitas los siguientes ejecutables instalados:
 #       xmlstarlet
-#       pdttotext
+
+PARSE="no"
+while true; do
+	read -p "Quieres solo descargar (D) o pasarlo a CSV tambien (C) o salir (otro)? " opt
+	case $opt in
+	        [Dd]* ) break;;
+	        [Cc]* ) PARSE="yes";break;;
+	        * ) exit;;
+	esac
+done
 
 URL_Archivos_Padron="http://cdn.servel.cl/padronesauditados/archivos.xml"
 XML_Archivos_Padron="./archivos.xml"
@@ -20,8 +31,13 @@ do
         then
                 COMUNA=${BASH_REMATCH[1]}
                 ARCHIVO=${BASH_REMATCH[2]}
+		echo "Descargando ${COMUNA}.pdf ..."
                 wget -q "${BASE_Padron_PDF}${ARCHIVO}" -O "${COMUNA}.pdf"
-                /bin/bash ./parsear.sh "${COMUNA}.pdf" &
+		if [[ $PARSE == "yes" ]]
+		then
+			echo "Ahora comienza la transformacion de ${COMUNA}.pdf a un CSV."
+                	/bin/bash ./parsear.sh "${COMUNA}.pdf" &
+		fi
         fi
 done < "$DEL_Archivos_Padron"
 
